87667c6a5e3a96dc60242d3cf1054ade3901cf91
Guilherme Ferreira <guilherme.rfs00@gmail.com>
2021-05-22 10:57:21 -0300

    iniciando projeto

---
+++ ms-analise-proposta/src/main/java/com/itau/msanaliseproposta/MsAnalisePropostaApplication.java
@@ -0,0 +1,13 @@
+package com.itau.msanaliseproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAnalisePropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAnalisePropostaApplication.class, args);
+	}
+
+}
---
+++ ms-analise-proposta/src/test/java/com/itau/msanaliseproposta/MsAnalisePropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.msanaliseproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAnalisePropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
